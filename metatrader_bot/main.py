b983aa196aa019314d27bbd77465f1d5a2bc6b39
João Marcelo <joaomarceloav@gmail.com>
2022-05-10 10:19:11 -0300

    Include middleware handler for channel posts

--- metatrader_bot/main.py
+++ metatrader_bot/main.py
@@ -1,6 +1,6 @@
-from email import message_from_string
 import os
 import telebot
+from telebot import apihelper
 
 from dotenv import load_dotenv
 
@@ -8,7 +8,26 @@ from messages_re import convert_signal, get_match_dict
 
 load_dotenv()
 
+apihelper.ENABLE_MIDDLEWARE = True
 bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
+
+
+@bot.middleware_handler(update_types=['channel_post'])
+def print_channel_post_text(bot_instance, channel_post):
+    message = channel_post.text
+    bot.delete_message(channel_post.chat.id, channel_post.message_id)
+    try:
+        in_message = get_match_dict(message)
+        if in_message:
+            out_message = convert_signal(in_message)
+        else:
+            return message
+
+        bot.send_message(chat_id=int(os.getenv('CHANNEL_ID')), text=out_message)
+
+        return out_message
+    except:
+        pass
 
 
 @bot.message_handler(commands=['start'])
@@ -26,7 +45,7 @@ def ping(message):
     )
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler()
 def broadcast_signal(message):
     try:
         in_message = get_match_dict(message.text)
